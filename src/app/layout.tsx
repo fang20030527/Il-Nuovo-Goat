f97184d875292e4b9ad1cbade3ef8f89dd965736
fang20030527 <zhenyufang162@gmail.com>
2026-09-05 10:47:18 +0800

    fix: use correct Clarity project id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "ybrt3q3nuq");`}
+})(window, document, "clarity", "script", "ybrt3g3nuq");`}
         </Script>
       </body>
     </html>
